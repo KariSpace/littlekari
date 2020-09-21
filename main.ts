b9b9f3a5fd612f0d0f823bbee6caa5ede2e246ef
Misha Marinenko <0x77dev@protonmail.com>
2020-09-22 02:45:20 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,61 @@
 let strip = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB);
+strip.setBrightness(255);
+
+const isBlocked = (): boolean => (RobotCar_Keyestudio.Sonar.ping() < 20 || RobotCar_Keyestudio.IrSensors.isRightBlocked() || RobotCar_Keyestudio.IrSensors.isLeftBlocked());
+
+// Led Loop
 let offset = 0;
-basic.forever(function () {
-  offset++;
-  if(offset === 255) offset = 0;
-  strip.clear();
-  strip.showRainbow(offset, 255);
-  strip.setBrightness(255);
-  strip.rotate(offset);
-});
-basic.forever(function () {
-  if (RobotCar_Keyestudio.Sonar.ping() < 20 || RobotCar_Keyestudio.IrSensors.isRightBlocked() || RobotCar_Keyestudio.IrSensors.isLeftBlocked()){
-     RobotCar_Keyestudio.Motors.move(__internal.__speedPicker(-100));
-     if(RobotCar_Keyestudio.IrSensors.isRightBlocked()){
-         RobotCar_Keyestudio.Motors.rightMotor(-100)
-     }
-     if(RobotCar_Keyestudio.IrSensors.isLeftBlocked()){
-         RobotCar_Keyestudio.Motors.leftMotor(-100)
-     }
-  } else {
-      RobotCar_Keyestudio.Motors.move(__internal.__speedPicker(100));
+basic.forever(() => {
+  if(isBlocked()){
+      strip.setMatrixColor(0, 255, neopixel.colors(NeoPixelColors.Red));
+      strip.show();
+      RobotCar_Keyestudio.Leds.showRed();
+  }else{
+    offset++;
+    if(offset === 255) offset = 0;
+    strip.clear();
+    strip.showRainbow(offset, 255);
+    strip.rotate(offset);
+    RobotCar_Keyestudio.Leds.showGreen();
   }
+});
+
+// Motor Loop
+basic.forever(() => { 
+    if(isBlocked()){
+         basic.clearScreen();
+         basic.showIcon(IconNames.Angry);
+         basic.pause(500);
+         RobotCar_Keyestudio.Motors.move(__internal.__speedPicker(-100));
+         if(RobotCar_Keyestudio.IrSensors.isRightBlocked()){
+             RobotCar_Keyestudio.Motors.rightMotor(-100)
+         }
+         if(RobotCar_Keyestudio.IrSensors.isLeftBlocked()){
+             RobotCar_Keyestudio.Motors.leftMotor(-100)
+         }
+         return
+     } else {
+         RobotCar_Keyestudio.Motors.move(__internal.__speedPicker(100));
+     }
     
 });
 
-basic.forever(function () {
-  
-  RobotCar_Keyestudio.Leds.showRed();
-  basic.showIcon(IconNames.Heart);
-  basic.clearScreen();
-  basic.showIcon(IconNames.SmallHeart);
-  basic.clearScreen();
-  basic.showString("Happy Birthday, Kari");
-  basic.clearScreen();
+// Screen Loop
+basic.forever(() => {
+    if(!isBlocked()){
+      basic.clearScreen();
+      basic.showIcon(IconNames.Heart);
+      basic.clearScreen();
+      basic.showIcon(IconNames.SmallHeart);
+      basic.clearScreen();
+      basic.showIcon(IconNames.Heart);
+      basic.clearScreen();
+      basic.showString("Happy Birthday, Kari", 65);
+      basic.clearScreen();
+     } else {
+        basic.clearScreen();
+        basic.showIcon(IconNames.Angry);
+        basic.pause(100);
+        basic.showString("Let me free", 75);
+     }
 });
